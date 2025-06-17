0748f7d7a750a97e4dd4b99733286236dda66f3c
ChVAjayReddy <35838377+ChVAjayReddy@users.noreply.github.com>
2025-06-17 19:20:15 +0530

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 let choose=document.querySelectorAll(".bus button");
 let totaltickets=0;
 let clickans;
-let seatnumbers=[];
+let price;let finalgender;let mobile;let email;
+let confirmticket=document.getElementById("confirm");
+let seatnumbers=[];let passengerdata=[];
 let statement=document.getElementById("ticketsno");
 choose.forEach(btn => {
     btn.addEventListener("click", (event) => {
@@ -19,12 +21,14 @@ choose.forEach(btn => {
       document.getElementById(event.target.id).innerText =clickans;
     totaltickets++;
   statement.innerText=`You have selected ${totaltickets} tickets`;
+  confirmticket.innerText=`Pay ₹.${totaltickets*500}`;
 addseats();}
     else{document.getElementById(event.target.id).style.backgroundColor = "white";
         document.getElementById(event.target.id).innerText ='';
 event.target.value=0;
 totaltickets--;
   statement.innerText=`You have selected ${totaltickets} tickets`;
+  confirmticket.innerText=`Pay ₹.${totaltickets*500}`;
   passengerremove();
     }
 
@@ -35,16 +39,17 @@ function addseats(){
     k++;
     let m;
     let passenger=document.createElement("tr");
-    let psno=document.createElement("td");
-     let pnamer=document.createElement("td");let pnamers=document.createElement("input");pnamers.type="input"
-     let pmale=document.createElement("td");let pmales=document.createElement("input");pmales.type="checkbox";
-     let pfemale=document.createElement("td");let pfemales=document.createElement("input");pfemales.type="checkbox";
-     let pseatnum=document.createElement("td");pseatnum=clickans;
+    let psno=document.createElement("td");psno.id="sno";
+     let pnamer=document.createElement("td");
+     let pnamers=document.createElement("input");pnamers.type="text";pnamers.id="namep";
+     let pmale=document.createElement("td");let pmales=document.createElement("input");pmales.type="checkbox";pmale.value="0";
+     let pfemale=document.createElement("td");let pfemales=document.createElement("input");pfemales.type="checkbox";pfemale.value="1";
+     let pseatnum=document.createElement("td");pseatnum.id="seat";pseatnum.innerText=clickans;
          let passengerlist=document.getElementById("selectiontable");
    passengerlist.append(passenger);
    passenger.append(psno);
    passenger.append(pnamer);
-   pnamer.append(pnamers);
+   pnamer.appendChild(pnamers);
    passenger.append(pmale);
    pmale.append(pmales);
    passenger.append(pfemale);
@@ -74,3 +79,101 @@ function passengerremove(){
    }
 
 }
+let c;let d;
+
+
+confirmticket.addEventListener("click",function(){
+  if(totaltickets==0){
+    alert("Please select seats");
+    }
+    else{
+      let datatable=document.getElementById("selectiontable");
+      for (let c = 1; c < datatable.rows.length; c++) {
+
+    let row = datatable.rows[c];
+      let nameInput = row.cells[1].querySelector('input[type="text"]');
+       let gender=row.querySelectorAll('input[type="chechbox"]');
+//        gender.addEventListener('change', function ()  {
+       
+//       if(this.checked){ console.log('Checkbox is checked');
+//     // Perform actions when checked
+
+      
+       
+//   }else {
+//     console.log('Checkbox is not checked');
+//     // Perform actions when unchecked
+//   }
+// })
+
+    passengerdata.push({
+        sno: row.cells[0].innerText,
+          name: nameInput.value,
+        seat: row.cells[4].innerText,
+        gender:finalgender,
+         
+    });
+    mobile=document.getElementById("fname").value;
+    email=document.getElementById("lname").value;
+
+    
+}
+console.log(passengerdata);
+
+
+      let  div = document.getElementById("buschart");
+    if (div.style.display === "none") {
+      div.style.display = "block"; // 
+    } else {
+      div.style.display = "none";
+    }
+     document.getElementById("ticketsno").style.display="none";
+      document.getElementById("selectiontable").style.display="none";
+       document.getElementById("confirm").style.display="none";
+       document.getElementById('contact').style.display="none";
+       document.getElementById("lname").style.display="none";
+       document.getElementById("front").style.display="none";
+       let finaldis=document.getElementById("final");
+       let fdis=document.createElement("h2");
+       fdis.innerText="Ticket Details";
+       finaldis.append(fdis);
+       let fname=document.createElement("p");
+       fname.innerText=`Dear ${passengerdata[0].name},\nThanks for choosing our travel agency.\n Passenger Details:`;
+
+        finaldis.append(fname)
+        let finaltable=document.createElement("table");finaltable.id="final"
+        finaldis.append(finaltable)
+        let e=0;
+        for(e=0;e<=passengerdata.length;e++){
+          let fpassenger=document.createElement("tr");
+          let fpsno=document.createElement("td");
+          let ffpsno=document.createElement("td");
+          let fffpsno=document.createElement("td");
+          if(e==0){
+          fpsno.innerText="S.No";
+          ffpsno.innerText="Name of the passenger";
+          fffpsno.innerText="Seat Number";}
+          else{fpsno.innerText=passengerdata[e-1].sno;
+          ffpsno.innerText=passengerdata[e-1].name;
+          fffpsno.innerText=passengerdata[e-1].seat;
+
+          }
+          finaltable.append(fpassenger);
+          fpassenger.append(fpsno);
+          fpassenger.append(ffpsno);
+          fpassenger.append(fffpsno);
+
+                 }
+
+let mob=document.createElement("p");
+          
+mob.innerText=`Contact Details \n Mobile : ${mobile} \n Email : ${email}`;
+finaldis.append(mob);
+
+let fstate=document.createElement("p");
+fstate.innerText="\n Wish You a Happy Journey.\n \n Ajay Tours and Travels.";
+finaldis.append(fstate)
+
+    }
+  })
+  
